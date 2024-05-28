096ea5ea53eb93c9c523c4d20e7d266a4998c820
David Nanjila <nanjiladavid2@gmail.com>
2024-05-28 18:38:01 +0300

    Chore: Create the the initial code for the landing page

--- landingPage/views.py
+++ landingPage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, Django!')
+    return render(request, 'home.html')
