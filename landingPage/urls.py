68b75addd9cd7491f80ddf4005dfb95eddc8737f
David Nanjila <nanjiladavid2@gmail.com>
2024-05-31 16:35:00 +0300

    Feat: Create a signup page

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('signup',views.signup, name='signup'),
 ]
--- landingPage/views.py
+++ landingPage/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'nam':'David'})
+
+def signup(request):
+    return render(request, 'signup.html')
